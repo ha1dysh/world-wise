d708195a1d469c95eb26646cf7902ba936c3446f
Oleksandr Haidysh <OleksHaid@gmail.com>
2023-08-07 19:18:38 +0300

    sass, styles, home-page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,22 @@
-import styles from "./page.module.css";
+import Link from "next/link";
+import styles from "./page.module.scss";
 
 export default function Home() {
-	return <main>hello</main>;
+	return (
+		<div className={styles.homepage}>
+			<main>
+				<h1>
+					You travel the world.
+					<br />
+					WorldWise keeps track of your adventures.
+				</h1>
+				<h2>
+					A world map that tracks your footsteps into every city you
+					can think of. Never forget your wonderful experiences, and
+					show your friends how you have wandered the world.
+				</h2>
+				<Link href='/main/cities'>Start tracking now</Link>
+			</main>
+		</div>
+	);
 }
