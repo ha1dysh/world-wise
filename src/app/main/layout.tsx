784d7351b7852c610835aa1d66d6dd181e5f3d71
Oleksandr Haidysh <OleksHaid@gmail.com>
2023-08-19 20:42:31 +0300

    quick fix

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import type { Metadata } from "next";
 import Sidebar from "@/components/layout/sidebar/sidebar";
 import styles from "./layout.module.scss";
 import { useEffect, useState } from "react";
@@ -10,11 +9,6 @@ const Map = dynamic(() => import("@/components/layout/map/map"), {
 	loading: () => <p>loading...</p>,
 	ssr: false,
 });
-
-export const metadata: Metadata = {
-	title: "World Wise | Map",
-	description: "World Wise Application",
-};
 
 export default function MainLayout({
 	children,
